26eaa8b05cc14a1b3fa08e2c9ed3aa95d2666964
ChiaYuYuChia <t1423314243@gmail.com>
2025-01-08 15:42:29 +0800

    add new project

---
+++ AlertPackage/AlertPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertPackageApp.swift
+//  AlertPackage
+//
+//  Created by ChiaYu Chang on 2025/1/8.
+//
+
+import SwiftUI
+
+@main
+struct AlertPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertPackage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlertPackage
+//
+//  Created by ChiaYu Chang on 2025/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
